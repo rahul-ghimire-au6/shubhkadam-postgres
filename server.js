19a851a687b38e8b868a70d0059268248d2230de
rahulghimire <ghimirerahul@gmail.com>
2020-04-11 18:42:53 +0530

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = require("./app")
 
 
 
-let Port = process.env.port || 5555
+let Port = process.env.PORT || 5555
 app.listen(Port, () => {
     console.log(`Server listening at ${Port}`)
 })
